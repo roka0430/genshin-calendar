20fdef7ee3b61e973aa0874866e81ae37c5c980f
Roka0430 <noxx0430@gmail.com>
2026-07-24 06:02:04 +0900

    feat: migrate views to ejs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import path from "path";
 import birthday from "./api/birthday.js";
+
+const __dirname = import.meta.dirname;
+const __filename = import.meta.filename;
 
 const app = express();
 const PORT = 3001;
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+
 app.use(express.static("public"));
 app.use("/api/birthday", birthday);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(PORT);
