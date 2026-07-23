05227c4ab6f17840c2840f8acf07bba74ff3db0d
Roka0430 <noxx0430@gmail.com>
2026-07-23 16:49:42 +0900

    feat: add birthday lookup by month

--- src/api/birthday.ts
+++ src/api/birthday.ts
@@ -1,30 +1,45 @@
-import {Router} from "express";
+import { Router } from "express";
 import fs from "fs";
-import path from "path"
+import path from "path";
 import * as yaml from "js-yaml";
+import { match } from "assert";
 
 const router = Router();
 
-const __dirname  = import.meta.dirname;
-const __filename  = import.meta.filename;
+const __dirname = import.meta.dirname;
+const __filename = import.meta.filename;
 
-const filePath = path.join(
-  __dirname,
-  "../../data/birthdays.yaml"
-);
+const filePath = path.join(__dirname, "../../data/birthdays.yaml");
 
 interface Birthday {
-  name: string,
+  name: string;
   birthday: {
-    month: number,
-    date: number,
-  }
+    month: number;
+    date: number;
+  };
 }
 
-router.get("/", (req,res) => {
-  const file = fs.readFileSync(filePath, "utf8");
-  const birthdays = yaml.load(file) as Birthday[];
+const file = fs.readFileSync(filePath, "utf8");
+const birthdays = yaml.load(file) as Birthday[];
+
+router.get("/", (req, res) => {
   res.json(birthdays);
-})
+});
+
+router.get("/:month", (req, res) => {
+  const month: number = Number(req.params.month);
+
+  if (!Number.isInteger(month) || month < 1 || month > 12) {
+    return res.status(400).json({
+      message: "Invalid month",
+    });
+  }
+
+  const matched: Birthday[] = birthdays.filter((birthday) => {
+    return birthday.birthday.month === month;
+  });
+
+  res.json(matched);
+});
 
 export default router;
